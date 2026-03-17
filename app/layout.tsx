a8ac03364253789bd1792250e22f622dc0768bb0
hiroki-sato-dev <hirokichiponpin@gmail.com>
2026-03-17 19:10:29 +0900

    タイポ修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "share-calender",
+  title: "share-calendar",
   description: "シェアしてる物の使用予定が一目でわかるカレンダーアプリ",
 };
 
